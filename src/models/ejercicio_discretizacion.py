520c38f4a9ee46f91faa3ac77ef35afa7e980010
Cesar De Alba <cesar.dealba@contpaqi.com>
2023-04-17 21:24:21 -0600

    Avances

--- src/models/ejercicio_discretizacion.py
+++ src/models/ejercicio_discretizacion.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from funpymodeling.exploratory import freq_tbl, status
+from sklearn.model_selection import train_test_split 
+import pickle
 
 # Para este caso nos interesa visualizar todas las columnas
 pd.set_option('display.max_columns', None)
@@ -18,8 +20,36 @@ ingreso_lab=pd.qcut(data1['ingreso_total_lab'], q=10, duplicates='drop')
 ingreso_nolab=pd.qcut(data1['ingreso_total_no_lab'], q=4, duplicates='drop')
 edad1=pd.cut(data1['edad'], bins=5)
 
-data1['comuna']=data1['comuna'].astype(str)
-data1['nhogar']=data1['nhogar'].astype(str)
-data1['años_escolaridad']=data1['años_escolaridad'].replace("Ningun año de escolaridad aprobado", '0')
-data1['años_escolaridad']=data1['años_escolaridad'].astype(float).astype("Int32")
-años_esc=pd.cut(data1['años_escolaridad'], q=5, duplicates='drop')
+data['comuna']=data['comuna'].astype(str)
+data['nhogar']=data['nhogar'].astype(str)
+data['años_escolaridad']=data['años_escolaridad'].replace("Ningun año de escolaridad aprobado", '0')
+data['años_escolaridad']=data['años_escolaridad'].astype(float).astype("Int32")
+años_esc=pd.cut(data1['años_escolaridad'], bins=5)
+
+# Eliminar filas que contengan NAN, en las columnas especificadas
+data=data.dropna(subset=['situacion_conyugal', 'sector_educativo', 'lugar_nacimiento', 'afiliacion_salud'])
+data=data.reset_index(drop=True)
+## Separar data set
+data_tr, data_ts = train_test_split(data, test_size=0.2)
+## Corte en el training
+años_tr, saved_bins = pd.qcut(data_tr['años_escolaridad'],
+                               q=5,
+                               retbins=True) # importante!
+
+# Usar los q guardados para discretizar el conjunto de test. ¿Porque no discretizar completo y luego separar?
+años_test=pd.cut(data_ts['años_escolaridad'],bins=saved_bins,  include_lowest=True)
+## En la práctica
+status(data)
+data['años_escolaridad']
+data['años_escolaridad2'], saved_bins = pd.qcut(data['años_escolaridad'], q=5, retbins=True)
+status(data['años_escolaridad2'])
+data['años_escolaridad2']=data['años_escolaridad2'].cat.add_categories("desconocido")
+data['años_escolaridad2']=data['años_escolaridad2'].fillna(value="desconocido")
+data=data.drop(['años_escolaridad'],axis=1)
+#data=data.fillna({'nivel_max_educativo':'desconocido'}, inplace=True)
+#status(data['nivel_max_educativo'])
+## One hot encoding
+data2=pd.get_dummies(data)
+status(data)
+with open('categories_ohe.pickle', 'wb') as handle:
+    pickle.dump(data2.columns, handle, protocol=pickle.HIGHEST_PROTOCOL)
